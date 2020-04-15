d5278bd456ec8f85c8c1bed0d332c6c58f0ab932
GRUNBLATT Remy <remy@grunblatt.org>
2020-04-15 17:56:13 +0200

    First commit - testing

---
+++ plugin.rb
@@ -0,0 +1,4 @@
+# name: read-only-username-email-account-creation
+# about: A simple plugin to make the username and emails field readonly in account creation
+# version: 0.0.1
+# authors: Rémy Grünblatt <remy@grunblatt.org>
